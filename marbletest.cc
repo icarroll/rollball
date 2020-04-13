eaf78554691035ba0faa2dd50db050a11a8b733d
Isaac Carroll <icarroll@pobox.com>
2020-04-13 16:03:41 -0700

    added second passive marble

--- marbletest.cc
+++ marbletest.cc
@@ -65,7 +65,6 @@ void init() {
                                SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
     if (gWindow == NULL) die("window");
 
-    //memset(& gContext, 0, sizeof(gContext));
     gContext = SDL_GL_CreateContext(gWindow);
     if (! gContext) die("gl context");
 
@@ -110,46 +109,6 @@ GLuint marbleShaderProgram;
 GLuint groundShaderProgram;
 
 void setup_shaders() {
-    /*
-    const char * vertex_shader_code =
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "layout (location = 1) in vec3 aNormal;\n"
-        "out vec3 FragPos;\n"
-        "out vec3 Normal;\n"
-        "uniform vec3 camera_pos;\n"
-        "uniform vec3 up_vector;\n"
-        "uniform vec3 obj_pos;\n"
-        "uniform mat4 projection;\n"
-        "uniform mat4 view;\n"
-        "uniform mat4 model;\n"
-        "mat4 rotationMatrix(vec3 axis, float angle)\n"
-        "{\n"
-        "  axis = normalize(axis);\n"
-        "  float s = sin(angle);\n"
-        "  float c = cos(angle);\n"
-        "  float oc = 1.0 - c;\n"
-        "\n"
-        "  return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,\n"
-        "              oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,\n"
-        "              oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,\n"
-        "              0.0,                                0.0,                                0.0,                                1.0);\n"
-        "}\n"
-        "void main() {\n"
-        "  FragPos = aPos + vec3(0.5,0.5,0);\n"
-        "  Normal = mat3(transpose(inverse(model))) * aNormal;\n"
-
-        "  vec3 look = normalize(obj_pos - camera_pos);\n"
-        "  vec3 rot_axis = cross(look, normalize(aNormal));\n"
-        "  float rot_angle = asin(length(rot_axis));\n"
-        "  if (dot(look, normalize(aNormal)) < 0) {\n"
-        "    rot_angle = -rot_angle;\n"
-        "    FragPos.x = 1-FragPos.x;\n"
-        "  }\n"
-        "  mat4 billboard = rotationMatrix(rot_axis, rot_angle);\n"
-        "  gl_Position = projection * view * model * billboard * vec4(aPos, 1.0);\n"
-        "}";
-    */
     // marble vertex shader
     ifstream bbvertfile("marbletest_billboard_vert.glsl");
     bbvertfile.seekg(0, ios::end);
@@ -204,14 +163,6 @@ void setup_shaders() {
     shaderfile.read(fragment_shader_code, length);
     fragment_shader_code[length] = '\0';
     shaderfile.close();
-    /*
-    const char * fragment_shader_code = 
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main() {\n"
-        "  FragColor = vec4(1,1,1,1);\n"
-        "}";
-    */
 
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -240,6 +191,8 @@ void setup_shaders() {
     }
 
     // ground vertex shader
+    // pass world coordinate to geometry shader
+    // transform world coordinate to screen position
     const char * ground_vertex_shader_code =
         "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
@@ -354,6 +307,7 @@ float ground_heights[GH_LENGTH][GH_WIDTH] = {
 rp3d::DynamicsWorld * world;
 rp3d::RigidBody * ground_body;
 rp3d::RigidBody * marble_body;
+rp3d::RigidBody * marble2_body;
 rp3d::RigidBody * camera_body;
 
 GLuint ground_vao;
@@ -364,6 +318,7 @@ GLuint marble_vao;
 rp3d::Vector3 gravity(0.0, -9.81, 0.0);
 
 rp3d::Vector3 SPAWN_POS(0.0, 3.5, -0.1);
+rp3d::Vector3 SPAWN_POS2(0.0, 2.5, -0.1);
 
 vector<float> ground_vertices = {};
 vector<int> ground_elements = {};
@@ -456,6 +411,19 @@ void setup_scene() {
     marble_mat.setFrictionCoefficient(rp3d::decimal(0.5));
     marble_body->setMaterial(marble_mat);
 
+{
+    // marble sphere 2
+    //rp3d::Transform marble_pose(rp3d::Vector3(-0.5, 3.5, -1.0), rp3d::Quaternion::identity());
+    rp3d::Transform marble_pose(SPAWN_POS2, rp3d::Quaternion::identity());
+    marble2_body = world->createRigidBody(marble_pose);
+    auto marble_shape = new rp3d::SphereShape(0.5);
+    marble2_body->addCollisionShape(marble_shape, rp3d::Transform(), 1.0);
+    auto marble_mat = marble2_body->getMaterial();
+    marble_mat.setBounciness(rp3d::decimal(0.5));
+    marble_mat.setFrictionCoefficient(rp3d::decimal(0.5));
+    marble2_body->setMaterial(marble_mat);
+}
+
     float points[] = {0.0f, 0.0f};
     GLuint vbo;
     glGenBuffers(1, & vbo);
@@ -465,36 +433,6 @@ void setup_scene() {
     glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), nullptr);
-
-    /*
-    float vertices[] = {
-        -0.5, -0.5, 0.0, 0.0,0.0,1.0,
-         0.5, -0.5, 0.0, 0.0,0.0,1.0,
-         0.5,  0.5, 0.0, 0.0,0.0,1.0,
-        -0.5,  0.5, 0.0, 0.0,0.0,1.0
-    };
-    glGenVertexArrays(1, & marble_vao);
-    GLuint vbo;
-    glGenBuffers(1, & vbo);
-    glBindVertexArray(marble_vao);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), nullptr);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void *) (3*sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    unsigned int elements[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-    GLuint ebo;
-    glGenBuffers(1, & ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
-    */
 }
 
 const float DIST = 6.0;
@@ -586,6 +524,17 @@ void physics_step(float dt) {
         marble_body->setTransform(t);
         marble_body->setLinearVelocity(rp3d::Vector3(0,0,0));
     }
+
+    // check for fall out 2
+{
+    rp3d::Transform t = marble2_body->getTransform();
+    rp3d::Vector3 pos = t.getPosition();
+    if (pos.y < -OUT || abs(pos.x) > OUT || abs(pos.y) > OUT) {
+        t.setPosition(SPAWN_POS);
+        marble2_body->setTransform(t);
+        marble2_body->setLinearVelocity(rp3d::Vector3(0,0,0));
+    }
+}
 }
 
 void draw_scene() {
@@ -700,6 +649,78 @@ void draw_scene() {
     //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glDrawArrays(GL_POINTS, 0, 1);
 
+    // marble sphere 2 marble boogaloo
+    auto marble2_tran = marble2_body->getTransform();
+    glm::vec3 marble2_pos = xlat(marble2_tran.getPosition());
+{
+    // set up uniform values
+    glUseProgram(marbleShaderProgram);
+    unsigned int projectionLoc = glGetUniformLocation(marbleShaderProgram, "projection");
+    unsigned int viewLoc = glGetUniformLocation(marbleShaderProgram, "view");
+    //unsigned int lightPosLoc = glGetUniformLocation(marbleShaderProgram, "lightPos");
+    //unsigned int lightColorLoc = glGetUniformLocation(marbleShaderProgram, "lightColor");
+
+    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+    //glUniform3f(lightPosLoc, 1.0, 1.0, -1.0);
+    //glUniform3fv(lightPosLoc, 1, glm::value_ptr(light_pos));
+    //glUniform3fv(lightColorLoc, 1, glm::value_ptr(light_color));
+
+    unsigned int cameraPosLoc = glGetUniformLocation(marbleShaderProgram, "cameraPos");
+    glUniform3fv(cameraPosLoc, 1, glm::value_ptr(camera_pos));
+
+    // draw marble
+    unsigned int camera_posLoc = glGetUniformLocation(marbleShaderProgram, "camera_pos");
+    glUniform3f(camera_posLoc, camera_pos.x, camera_pos.y, camera_pos.z);
+    unsigned int up_vectorLoc = glGetUniformLocation(marbleShaderProgram, "up_vector");
+    glUniform3f(up_vectorLoc, up_vector.x, up_vector.y, up_vector.z);
+
+    //cout << marble2_pos.x << ',' << marble2_pos.y << ',' << marble2_pos.z << endl;
+    unsigned int obj_posLoc = glGetUniformLocation(marbleShaderProgram, "obj_pos");
+    glUniform3fv(obj_posLoc, 1, glm::value_ptr(marble2_pos));
+
+    unsigned int camera_lookLoc = glGetUniformLocation(marbleShaderProgram, "camera_look");
+    glUniform3fv(camera_lookLoc, 1, glm::value_ptr(camera_look));
+    unsigned int camera_rightLoc = glGetUniformLocation(marbleShaderProgram, "camera_right");
+    glUniform3fv(camera_rightLoc, 1, glm::value_ptr(camera_right));
+    unsigned int camera_upLoc = glGetUniformLocation(marbleShaderProgram, "camera_up");
+    glUniform3fv(camera_upLoc, 1, glm::value_ptr(camera_up));
+
+    unsigned int aspectLoc = glGetUniformLocation(marbleShaderProgram, "aspect");
+    glUniform1f(aspectLoc, ASPECT);
+
+    auto model = glm::mat4(1.0f);
+    model = glm::translate(model, marble2_pos);
+    unsigned int modelLoc = glGetUniformLocation(marbleShaderProgram, "model");
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+    rp3d::Matrix3x3 tempm = marble2_tran.getOrientation().getMatrix();
+    glm::mat3 marble_rot;
+    for (int y=0 ; y<3 ; y+=1) {
+        for (int x=0 ; x<3 ; x+=1) {
+            marble_rot[y][x] = tempm[y][x];
+        }
+    }
+    unsigned int obj_rotationLoc = glGetUniformLocation(marbleShaderProgram, "obj_rotation");
+    glUniformMatrix3fv(obj_rotationLoc, 1, GL_FALSE, glm::value_ptr(marble_rot));
+
+    // color is ignored for this fragment shader
+    //unsigned int objectColorLoc = glGetUniformLocation(marbleShaderProgram, "objectColor");
+    //glm::vec3 color = {0.0, 1.0, 1.0};
+    //glUniform3fv(objectColorLoc, 1, glm::value_ptr(color));
+
+    /*
+    unsigned int iTimeLoc = glGetUniformLocation(marbleShaderProgram, "iTime");
+    glUniform1f(iTimeLoc, frame * (20.0 / 1000.0));
+    */
+
+    glBindVertexArray(marble_vao);
+    //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_POINTS, 0, 1);
+}
+
     // ground heightfield
     // set up uniform values
     glUseProgram(groundShaderProgram);
@@ -719,6 +740,11 @@ void draw_scene() {
     glUniform3fv(sphere_posLoc, 1, glm::value_ptr(marble_pos));
     unsigned int sphere_rLoc = glGetUniformLocation(groundShaderProgram, "sphere_r");
     glUniform1f(sphere_rLoc, 0.5);
+
+    unsigned int sphere2_posLoc = glGetUniformLocation(groundShaderProgram, "sphere2_pos");
+    glUniform3fv(sphere2_posLoc, 1, glm::value_ptr(marble2_pos));
+    unsigned int sphere2_rLoc = glGetUniformLocation(groundShaderProgram, "sphere2_r");
+    glUniform1f(sphere2_rLoc, 0.5);
 
     // draw ground
     auto g_model = glm::mat4(1.0f);
--- marbletest_ground.glsl
+++ marbletest_ground.glsl
@@ -8,6 +8,8 @@ uniform vec3 lightColor;
 uniform vec3 objectColor;
 uniform vec3 sphere_pos;
 uniform float sphere_r;
+uniform vec3 sphere2_pos;
+uniform float sphere2_r;
 void main() {
   float ambientStrength = 0.1;
   vec3 ambient = ambientStrength * lightColor;
@@ -27,12 +29,14 @@ void main() {
 
   vec2 spos = sphere_pos.xz;
   vec2 gpos = FragPos.xz;
+  vec2 s2pos = sphere2_pos.xz;
   float in_circle = max(0, (sphere_r - length(gpos - spos)) / sphere_r);
+  float in_circle2 = max(0, (sphere2_r - length(gpos - s2pos)) / sphere2_r);
   
   //vec3 result = (ambient + diffuse + specular) * objectColor;
   //vec3 result = (ambient + (diffuse + specular)*(1-in_circle)) * objectColor;
   vec3 result;
-  if (in_circle > 0.0) result = (ambient + diffuse/4) * objectColor;
+  if (in_circle > 0.0 || in_circle2 > 0.0) result = (ambient + diffuse/4) * objectColor;
   else result = (ambient + diffuse + specular) * objectColor;
 
   FragColor = vec4(result, 1.0);
